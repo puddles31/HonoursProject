734997f8329042ab2f11235c39a6734727e405cb
Samuel Borg <spb5@st-andrews.ac.uk>
2024-11-08 15:55:48 +0000

    oops forgot to populate the other databases

--- CubeAlgorithm/src/rubikscube/PopulatePatternDatabases.java
+++ CubeAlgorithm/src/rubikscube/PopulatePatternDatabases.java
@@ -10,6 +10,8 @@ public class PopulatePatternDatabases {
     
     public static void main(String[] args) {
         populateCornerDatabase();
+        populateFirstEdgeDatabase();
+        populateSecondEdgeDatabase();
     }
 
 
