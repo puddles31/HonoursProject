70bb8e88c91ce5fbb2e646a6b8a3eb1ea28429e3
Sam Borg <spb5@st-andrews.ac.uk>
2025-01-14 14:00:58 +0000

    Added Kilominx print state function (need to test)

--- TwistyPuzzleSolvers/src/kilominx/Kilominx.java
+++ TwistyPuzzleSolvers/src/kilominx/Kilominx.java
@@ -1,7 +1,5 @@
 package kilominx;
 
-import rubikscube.Cube;
-
 /**
  * A Kilominx, represented as an array of "kubies" (Kilominx cubies).
  * @see #Kilominx()
@@ -13,9 +11,10 @@ public class Kilominx {
 
     /**
      * Colours on the Kilominx.
+     * White, Dark Green, Red, Dark Blue, Yellow, Purple, Light Blue, Beige, Pink, Light Green, Orange, Grey
      */
     public static enum Colour {
-        W, E, R, B, V, Y, C, P, T, L, O, G
+        Wh, DG, Re, DB, Ye, Pu, LB, Be, Pi, LG, Or, Gy
     };
 
     /**
@@ -37,29 +36,51 @@ public class Kilominx {
     }
 
     // Kubies are indexed from 0 to 19 in the following order:
-    // DEFAULT KUBIE INDEXING HERE
-    final byte KUBIE_UF;
+    // Todo: Figure out what correct index order should be
+    final byte KUBIE_UFL = 0,
+               KUBIE_UFR = 1,
+               KUBIE_UBL = 2,
+               KUBIE_UBR = 3,
+               KUBIE_UBM = 4,
+               KUBIE_MFL = 5,
+               KUBIE_FMD = 6,
+               KUBIE_MFR = 7,
+               KUBIE_FRD = 8,
+               KUBIE_MBR = 9,
+               KUBIE_BRD = 10,
+               KUBIE_MBM = 11,
+               KUBIE_BLD = 12,
+               KUBIE_MBL = 13,
+               KUBIE_FLD = 14,
+               KUBIE_DFM = 15,
+               KUBIE_DFL = 16,
+               KUBIE_DFR = 17,
+               KUBIE_DBL = 18,
+               KUBIE_DBR = 19;
 
 
     /**
      * A cubie on the Kilominx (aka a Kubie).
-     * E kubie has an index (which represents which colours are on the kubie's sides)
+     * A kubie has an index (which represents which colours are on the kubie's sides)
      * and an orientation (0, 1, or 2).
      * @see #Kubie(byte, byte)
      * @see #Kilominx
      */
     class Kubie {
         // Kubies are indexed from 0 to 19 in the following order, with the following initial colours:
+        
+        // 0,      1,      2,      3,      4,      5,      6,      7,      8,      9,      
+        // UFL,    UFR,    UBL,    UBR,    UBM,    MFL,    FMD,    MFR,    FRD,    MBR,    
+        // WhDGRe, WhReDB, WhDGPu, WhDbYe, WhPuYe, DGReBe, ReBePi, ReDBPi, DBPiLG, DBYeLG, 
 
-        // INDEX LIST GOES HERE
-
-        // (note: kubies that are an odd number of quarter turns away are also an odd number of indices apart_
-        // NEED TO VERIFY THIS AND PROVIDE EXAMPLE 
+        // 10,     11,     12,     13,     14,     15,     16,     17,     18,     19
+        // BRD,    MBM,    BLD,    MBL,    FLD,    DFM,    DFL,    DFR,    DBL,    DBR
+        // YeLGOr, YePuOr, PuOrLB, PuDGLB, DGLBBe, BePiGr, LBBeGr, PiLGGr, OrLBGr, LGOrGr
 
         // Kubies have an orientation of:
-        //  - 0 (),
-        //  - 1 (),
-        //  - or 2 ()
+        //  - 0 (oriented),
+        //  - 1 (colours turned CW),
+        //  - or 2 (colours turned CCW)
         private byte index, orientation;
 
         /**
@@ -85,7 +106,10 @@ public class Kilominx {
      * @see #Kubie(byte, byte)
      */
     public Kilominx() {
-        
+        // Initialize kubies
+        for (byte i = 0; i < 20; i++) {
+            kubies[i] = new Kubie(i, (byte) 0);
+        }
     }
     
     /**
@@ -93,8 +117,13 @@ public class Kilominx {
      * The Kilominx is initialised with the same kubie indices/orientations as the given Kilominx.
      * @param other - The Kilominx to copy.
      */
-    public Kilominx(Cube other) {
-        System.out.println("Not implemented yet");
+    public Kilominx(Kilominx other) {
+        byte[] otherIndices = other.getKubieIndices();
+        byte[] otherOrientations = other.getKubieOrientations();
+
+        for (byte i = 0; i < 20; i++) {
+            kubies[i] = new Kubie(otherIndices[i], otherOrientations[i]);
+        }
     }
 
     
@@ -104,8 +133,13 @@ public class Kilominx {
      * @see #getKubieOrientations()
      */
     public byte[] getKubieIndices() {
-        System.out.println("Not implemented yet");
-        return null;
+        byte[] indices = new byte[20];
+
+        for (byte i = 0; i < 20; i++) {
+            indices[i] = kubies[i].index;
+        }
+
+        return indices;
     }
 
     /**
@@ -114,8 +148,13 @@ public class Kilominx {
      * @see #getKubieIndices()
      */
     public byte[] getKubieOrientations() {
-        System.out.println("Not implemented yet");
-        return null;
+        byte[] orientations = new byte[20];
+
+        for (byte i = 0; i < 20; i++) {
+            orientations[i] = kubies[i].orientation;
+        }
+
+        return orientations;
     }
 
 
@@ -124,8 +163,14 @@ public class Kilominx {
      * @return {@code true} if the Kilominx is solved, {@code false} otherwise.
      */
     public boolean isSolved() {
-        System.out.println("Not implemented yet");
-        return false;
+        // Check that all kubies are in the position and orientation
+        for (byte i = 0; i < 20; i++) {
+            if (kubies[i].index != i || kubies[i].orientation != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 
@@ -135,14 +180,26 @@ public class Kilominx {
      * @param incr - The amount to increase the orientation by.
      */
     private void increaseKubieOrientation(byte posIndex, byte incr) {
-        System.out.println("Not implemented yet");
+        Kubie kubie = kubies[posIndex];
+
+        kubie.orientation += incr;
+
+        // faster equivalent to kubie.orientation = (kubie.orientation + incr) % 3
+        if (kubie.orientation == 3) {
+            kubie.orientation = 0;
+        }
+        else if (kubie.orientation == 4) {
+            kubie.orientation = 1;
+        }
     }
 
 
     /**
      * Get the colours of a kubie.
      * @param posIndex - The position index of the kubie.
-     * @return An array of the colours of the kubie. The colours are in the order ?????
+     * @return An array of the colours of the kubie.
+     * The colours are in the order top/bottom, up-front-left/down-back-right, up-front-middle/down-back-middle, 
+     * up-front-right/down-back-left, down-front-left/up-back-right, down-front-right/up-back-left.
      */
     private Colour[] getKubieColours(byte posIndex) {
         System.out.println("Not implemented yet");
@@ -178,7 +235,88 @@ public class Kilominx {
     /**
      * Print the Kilominx state in a human-readable format.
      */
-    public void printCubeState() {
-        System.out.println("Not implemented yet");
+    public void printKilominxState() {
+        
+        // Print top face
+        System.out.print("                           ");
+        System.out.println(getKubieColours(KUBIE_UBM)[0]);
+        System.out.print("                        ");
+        System.out.print(getKubieColours(KUBIE_UBL)[0] + "    ");
+        System.out.println(getKubieColours(KUBIE_UBR)[0]);
+        System.out.print("                         ");
+        System.out.print(getKubieColours(KUBIE_UFL)[0] + "  ");
+        System.out.println(getKubieColours(KUBIE_UFR)[0] + "\n");
+
+        // Print upper row of upper-middle faces
+        System.out.print(" " + getKubieColours(KUBIE_UBM)[2]);
+        System.out.print("  " + getKubieColours(KUBIE_UBL)[2]);
+        System.out.print("      " + getKubieColours(KUBIE_UBL)[1]);
+        System.out.print("  " + getKubieColours(KUBIE_UFL)[1]);
+        System.out.print("      " + getKubieColours(KUBIE_UFL)[2]);
+        System.out.print("  " + getKubieColours(KUBIE_UFR)[1]);
+        System.out.print("      " + getKubieColours(KUBIE_UFR)[2]);
+        System.out.print("  " + getKubieColours(KUBIE_UBR)[1]);
+        System.out.print("      " + getKubieColours(KUBIE_UBR)[2]);
+        System.out.println("  " + getKubieColours(KUBIE_UBM)[1]);
+
+        // Print middle row of upper-middle faces
+        System.out.print(getKubieColours(KUBIE_MBM)[2] + "    ");
+        System.out.print(getKubieColours(KUBIE_MBL)[2] + "    ");
+        System.out.print(getKubieColours(KUBIE_MBL)[0] + "    ");
+        System.out.print(getKubieColours(KUBIE_MFL)[0] + "    ");
+        System.out.print(getKubieColours(KUBIE_MFL)[1] + "    ");
+        System.out.print(getKubieColours(KUBIE_MFR)[0] + "    ");
+        System.out.print(getKubieColours(KUBIE_MFR)[1] + "    ");
+        System.out.print(getKubieColours(KUBIE_MBR)[1] + "    ");
+        System.out.print(getKubieColours(KUBIE_MBR)[2] + "    ");
+        System.out.println(getKubieColours(KUBIE_MBM)[1] + "    ");
+
+        // Print bottom row of upper-middle faces
+        System.out.print("   " + getKubieColours(KUBIE_BLD)[2]);
+        System.out.print("          " + getKubieColours(KUBIE_FLD)[0]);
+        System.out.print("          " + getKubieColours(KUBIE_FMD)[0]);
+        System.out.print("          " + getKubieColours(KUBIE_FRD)[1]);
+        System.out.println("          " + getKubieColours(KUBIE_BRD)[2] + "\n");
+
+        // Print upper row of lower-middle faces
+        System.out.print("         " + getKubieColours(KUBIE_MBL)[1]);
+        System.out.print("          " + getKubieColours(KUBIE_MFL)[2]);
+        System.out.print("          " + getKubieColours(KUBIE_MFR)[2]);
+        System.out.print("          " + getKubieColours(KUBIE_MBR)[0]);
+        System.out.println("          " + getKubieColours(KUBIE_MBM)[0]);
+
+        // Print middle row of lower-middle faces
+        System.out.print("      " + getKubieColours(KUBIE_BLD)[1]);
+        System.out.print("    " + getKubieColours(KUBIE_FLD)[1]);
+        System.out.print("    " + getKubieColours(KUBIE_FLD)[2]);
+        System.out.print("    " + getKubieColours(KUBIE_FMD)[1]);
+        System.out.print("    " + getKubieColours(KUBIE_FMD)[2]);
+        System.out.print("    " + getKubieColours(KUBIE_FRD)[2]);
+        System.out.print("    " + getKubieColours(KUBIE_FRD)[0]);
+        System.out.print("    " + getKubieColours(KUBIE_BRD)[0]);
+        System.out.print("    " + getKubieColours(KUBIE_BRD)[1]);
+        System.out.println("    " + getKubieColours(KUBIE_BLD)[0]);
+
+        // Print bottom row of lower-middle faces
+        System.out.print("       " + getKubieColours(KUBIE_DBL)[2]);
+        System.out.print("  " + getKubieColours(KUBIE_DFL)[1]);
+        System.out.print("      " + getKubieColours(KUBIE_DFL)[2]);
+        System.out.print("  " + getKubieColours(KUBIE_DFM)[1]);
+        System.out.print("      " + getKubieColours(KUBIE_DFM)[2]);
+        System.out.print("  " + getKubieColours(KUBIE_DFR)[2]);
+        System.out.print("      " + getKubieColours(KUBIE_DFR)[1]);
+        System.out.print("  " + getKubieColours(KUBIE_DBR)[1]);
+        System.out.print("      " + getKubieColours(KUBIE_DBR)[2]);
+        System.out.println("  " + getKubieColours(KUBIE_DBL)[1] + "\n");
+
+        // Print bottom face
+        System.out.print("                           ");
+        System.out.println(getKubieColours(KUBIE_DFM)[0]);
+        System.out.print("                        ");
+        System.out.print(getKubieColours(KUBIE_DFL)[0] + "    ");
+        System.out.println(getKubieColours(KUBIE_DFR)[0]);
+        System.out.print("                         ");
+        System.out.print(getKubieColours(KUBIE_DBL)[0] + "  ");
+        System.out.println(getKubieColours(KUBIE_DBR)[0] + "\n");        
     }
 }
