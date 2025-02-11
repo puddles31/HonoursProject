7e384c7f6979923c0eff850e2e24133509937bac
Samuel Borg <spb5@st-andrews.ac.uk>
2025-02-11 15:31:54 +0000

    Fixed kilominx script

--- TwistyPuzzleSolvers/kilominx.sh
+++ TwistyPuzzleSolvers/kilominx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-javac -d TwistyPuzzleSolvers/classes TwistyPuzzleSolvers/src/kilominx/*.java
-java -cp TwistyPuzzleSolvers/classes kilominx.KilominxInteractive
+javac -d TwistyPuzzleSolvers/classes TwistyPuzzleSolvers/src/kilominx/*.java TwistyPuzzleSolvers/src/kilominx/interactive/*.java
+java -cp TwistyPuzzleSolvers/classes kilominx.interactive.KilominxTerminal
